0ac4fc5a4525b07edfd998585c8c354e5fda5a60
clin94118 <clin@ccsf.edu>
2024-01-03 21:06:02 -0800

    fix: udpated app requirements.txt for python 3.10

--- web.py
+++ web.py
@@ -24,7 +24,7 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.rerun()
 
-st.text_input(label="Enter a todo: ", placeholder="Add new todo... ",
+st.text_input(label="Enter a todo: ", label_visibility='hidden', placeholder="Add new todo... ",
               on_change=add_todo, key="new_todo")
 
 # st.session_state
